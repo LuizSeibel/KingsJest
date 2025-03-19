d3bc6902d5daab8e14676b4498777dbe9d27555a
LuizSeibel <luizfelipedasilvaseibel@gmail.com>
2025-03-19 16:32:18 -0300

    Organizacão do projeto

--- KingsJest/AppDelegate.swift
+++ KingsJest/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,8 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: ContentView())
+        self.window = window
+        window.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ KingsJest/View/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  KingsJest
+//
+//  Created by Luiz Seibel on 19/03/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
