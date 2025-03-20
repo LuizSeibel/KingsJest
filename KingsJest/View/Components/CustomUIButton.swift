65dfe184a25e06bfdb0bbeee015515f054f53668
LuizSeibel <luizfelipedasilvaseibel@gmail.com>
2025-03-20 17:49:49 -0300

    a

---
+++ KingsJest/View/Components/CustomUIButton.swift
@@ -0,0 +1,28 @@
+//
+//  CustomUIButton.swift
+//  KingsJest
+//
+//  Created by Luiz Seibel on 20/03/25.
+//
+
+import SwiftUI
+
+struct CustomUIButton: View {
+    
+    var body: some View {
+        VStack{
+            Text("Hello, World!")
+            
+            Button(action: {
+                
+            }, label: {
+                Text("fONTS")
+                
+            })
+        }
+    }
+}
+
+#Preview {
+    CustomUIButton()
+}
