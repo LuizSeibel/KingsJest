515f6e720d115a09809211619d534f29c494bb33
LuizSeibel <luizfelipedasilvaseibel@gmail.com>
2025-03-20 17:02:58 -0300

    Criação do textfield personalizado para o nickname

---
+++ KingsJest/View/Components/Nickname.swift
@@ -0,0 +1,86 @@
+//
+//  Nickname.swift
+//  KingsJest
+//
+//  Created by Luiz Seibel on 20/03/25.
+//
+
+import SwiftUI
+import UIKit
+
+struct Nickname: View {
+    @Binding var text: String
+    @State var placeholder: String = "Nickname"
+    
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(Color(.systemGray6))
+                .frame(width: 30, height: 30)
+                .overlay(
+                    Image("touca")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(3)
+                )
+            
+            CustomUITextField(placeholder: placeholder, text: $text)
+                .padding(5)
+                .font(.system(size: 24, weight: .bold))
+                .foregroundStyle(Color(.systemGray4))
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 12)
+        .background(
+            RoundedRectangle(cornerRadius: 18)
+                .fill(Color("UIBackground1"))
+        )
+        .frame(maxWidth: 200, maxHeight: 5)
+    }
+}
+
+#Preview {
+    Nickname(text: .constant(""))
+}
+
+struct CustomUITextField: UIViewRepresentable {
+    var placeholder: String
+    @Binding var text: String
+    
+    var placeholderColor: UIColor = .gray
+    var font: UIFont = .systemFont(ofSize: 14, weight: .bold)
+    
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        textField.text = text
+        textField.font = font
+        textField.textColor = UIColor.systemGray6
+        textField.autocorrectionType = .no
+        textField.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [NSAttributedString.Key.foregroundColor: placeholderColor]
+        )
+        return textField
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: CustomUITextField
+
+        init(_ textField: CustomUITextField) {
+            self.parent = textField
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            parent.text = textField.text ?? ""
+        }
+    }
+}
