4b4a599e52f9284440b27ef49c610d26cc176548
Ollieveira1 <willysaaoliveira@gmail.com>
2025-03-26 09:47:28 -0300

    - implementacao de um controlador de cenas de jogo

--- KingsJest/ViewModel/ScenesController.swift
+++ KingsJest/ViewModel/ScenesController.swift
@@ -5,4 +5,35 @@
 //  Created by Willys Oliveira on 26/03/25.
 //
 
-import Foundation
+import SwiftUI
+import SpriteKit
+
+enum GameSceneType: String {
+    case phaseOne = "PhaseOne"
+}
+
+struct GameScenesViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    let sceneType: GameSceneType
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        let skView = SKView(frame: UIScreen.main.bounds)
+        viewController.view = skView
+        
+        if let scene = SKScene(fileNamed: sceneType.rawValue) {
+            scene.scaleMode = .resizeFill
+            skView.presentScene(scene)
+        }
+        
+        skView.ignoresSiblingOrder = false
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        // Atualizações futuras, se necessário
+    }
+}
