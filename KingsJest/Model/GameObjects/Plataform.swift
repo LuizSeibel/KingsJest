1f31a5c502e4c5fa9ecd3c77656919690fa6f0d9
Ollieveira1 <willysaaoliveira@gmail.com>
2025-04-02 10:11:08 -0300

    - add classe Plataform para gerenciar a fisica das plataformas via código

--- KingsJest/Model/GameObjects/Plataform.swift
+++ KingsJest/Model/GameObjects/Plataform.swift
@@ -5,4 +5,30 @@
 //  Created by Willys Oliveira on 02/04/25.
 //
 
-import Foundation
+import SpriteKit
+
+class Plataform {
+    var nodes: [SKSpriteNode] = []
+    
+    init (scene: SKScene) {
+        
+        for node in scene.children {
+            if node.name == "plataforma" {
+                if let plataformNode = node as? SKSpriteNode {
+                    setupPhysics(for: plataformNode)
+                    nodes.append(plataformNode)
+                }
+            }
+        }
+    }
+    
+    private func setupPhysics(for plataformNode: SKSpriteNode) {
+        plataformNode.physicsBody = SKPhysicsBody(texture: plataformNode.texture!, size: plataformNode.size)
+        plataformNode.physicsBody?.isDynamic = false
+        plataformNode.physicsBody?.friction = 0
+        plataformNode.physicsBody?.categoryBitMask = .plataform
+        plataformNode.physicsBody?.contactTestBitMask = 0
+        plataformNode.physicsBody?.collisionBitMask = .player
+    }
+
+}
